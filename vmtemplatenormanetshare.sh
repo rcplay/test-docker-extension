9b498c03d90035fac3d6edf940637f612195bef6
Riccardo <info@riccardoplaia.eu>
2017-06-19 17:27:27 +0200

    Add files via upload

--- vmtemplatenormanetshare.sh
+++ vmtemplatenormanetshare.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
-NAS =//cartografiashare.file.core.windows.net/colacino
-DIRNAS = /mnt/mountpoint
-USER=cartografiashare
-PASS=piFmBMa3KJ/YPmujCqBSaqADLrYu4hdG3sGTnJpdzR6Tl567z8xgL38bISgo5T3S4oXWP8BbREF01hMgz4ujxQ==
-
 sudo yum install samba-client samba-common cifs-utils
-sudo mkdir $DIRNAS
-sudo mount.cifs $NAS $DIRNAS -o user=$USER,password=$PASS,dir_mode=0777,file_mode=0777,serverino
-df -h $DIRNAS
+sudo mkdir /mnt/mountpoint
+sudo mount.cifs //cartografiashare.file.core.windows.net/colacino /mnt/mountpoint -o user=cartografiashare,password=piFmBMa3KJ/YPmujCqBSaqADLrYu4hdG3sGTnJpdzR6Tl567z8xgL38bISgo5T3S4oXWP8BbREF01hMgz4ujxQ==,dir_mode=0777,file_mode=0777,serverino
+df -h /mnt/mountpoint
